4feff032e83d61833dab505a3d4ad709ed6ea70f
me-khushii <168956864+Khushii412@users.noreply.github.com>
2026-02-22 20:07:40 +0530

    Add maxSubArray method using Kadane's algorithm
    Implement Kadane's algorithm to find the maximum subarray sum.

---
+++ dsa4.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        max_sum = nums[0]
+        current_sum = nums[0]
+        for i in range(1, len(nums)):
+            current_sum = max(current_sum + nums[i] , nums[i])
+            max_sum = max(current_sum, max_sum)
+        return max_sum    
+# maximum subarray of a array. 
+# kadane method. 
