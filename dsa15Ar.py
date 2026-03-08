fe6900efb2f7e44b10a6c01e04596e3bfdf8043f
me-khushii <168956864+Khushii412@users.noreply.github.com>
2026-03-08 11:36:28 +0530

    Optimize runningSum method for in-place calculation
    Refactor runningSum method to use in-place modification for O(1) space complexity.

--- dsa15Ar.py
+++ dsa15Ar.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution(object):  # o(n) time lookup
     def runningSum(self, nums):
         prefix_sum = 0
         result = []
@@ -10,3 +10,8 @@ class Solution(object):
 #[1,2,3,4]
 #Output
 #[1,3,6,10]
+class Solution(object):    # in place approach for 0(1) space lookup
+    def runningSum(self, nums):
+        for i in range(1, len(nums)):
+            nums[i] = nums[i]+nums[i-1]
+        return nums
